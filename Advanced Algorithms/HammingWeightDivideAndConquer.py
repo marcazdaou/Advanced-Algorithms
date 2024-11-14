ec29bb3283f451ecea565fe76f69924647cb6ddf
Marc Abi Zeid Daou <123509967+marcazdaou@users.noreply.github.com>
2024-11-14 14:26:58 -0500

    Update HammingWeightDivideAndConquer.py

--- Advanced Algorithms/HammingWeightDivideAndConquer.py
+++ Advanced Algorithms/HammingWeightDivideAndConquer.py
@@ -28,23 +28,3 @@ end_time = time.time()
 print("Hamming weight:", hamming_weight)
 print("Execution time:", end_time - start_time, "seconds")
 
-"""
-import time
-def hamming_weight(n):  # for the sequential algorithm
-    count = 0
-    while n:
-        count += n & 1
-        n >>= 1
-    return count
-
-
-binary_representation = input("Enter the binary representation of an unsigned integer: ")
-
-start_time = time.time()  # Start time measurement
-decimal_value = int(binary_representation, 2)
-print("Hamming weight:", hamming_weight(decimal_value))
-end_time = time.time()  # End time measurement
-
-execution_time = end_time - start_time
-print("Execution time:", execution_time, "seconds")
-"""
